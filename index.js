02c70ea33eb9ee73e18aa79604f375a9fbb79dc4
franklindesmond131-hue <franklindesmond131@gmail.com>
2026-02-05 15:36:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages
+  ],
+});
+
+const OWNER_ID = "1085466254522974248";
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("guildMemberAdd", async (member) => {
+  try {
+    const owner = await client.users.fetch(OWNER_ID);
+
+    await owner.send(
+      `👋 New member joined **${member.guild.name}**:\n` +
+      `Username: **${member.user.tag}**\n` +
+      `User ID: ${member.user.id}`
+    );
+  } catch (err) {
+    console.log("Could not send DM:", err);
+  }
+});
+
+client.login(process.env.TOKEN);
